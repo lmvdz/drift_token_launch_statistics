e2254696a533ec54f4b3c4cf0a95ac2a0765ebeb
Lars van der Zande <lmvanderzande@gmail.com>
2024-05-17 14:10:48 -0500

    add uncliamed statistic

--- src/index.ts
+++ src/index.ts
@@ -10,25 +10,24 @@ config();
     let idl = await Program.fetchIdl('E7HtfkEMhmn9uwL7EFNydcXBWy5WCYN1vFmKKjipEH1x', provider)
     if (idl) {
         let program = new Program(idl, 'E7HtfkEMhmn9uwL7EFNydcXBWy5WCYN1vFmKKjipEH1x', provider)
-        // console.log(program.account)
         let merkle_distributors = await program.account['merkleDistributor'].all();
         let totalForgone = ([...merkle_distributors.values()].map(programAccount => {
-            // console.log((programAccount as any).account.totalAmountForgone)
             return (programAccount as any).account.totalAmountForgone
         }).reduce((a: BN, b: BN) => {
-            // console.log(a, b)
             return a.add(b)
         }, new BN(0)) as BN).div(new BN(Math.pow(10, 6)))
         let totalClaimed = ([...merkle_distributors.values()].map(programAccount => {
-            // console.log((programAccount as any).account.totalAmountForgone)
             return (programAccount as any).account.totalAmountClaimed
         }).reduce((a: BN, b: BN) => {
-            // console.log(a, b)
             return a.add(b)
         }, new BN(0)) as BN).div(new BN(Math.pow(10, 6)))
         console.log('Forgone', new Intl.NumberFormat("en-US", { style: 'currency', currency: 'USD' }).format(totalForgone.toNumber()).replace('$', 'DRIFT '))
         console.log('% Forgone', ((totalForgone.toNumber() / 120000000) * 100).toFixed(2) + '%' )
+        console.log()
         console.log('Claimed', new Intl.NumberFormat("en-US", { style: 'currency', currency: 'USD' }).format(totalClaimed.toNumber()).replace('$', 'DRIFT '))
         console.log('% Claimed', ((totalClaimed.toNumber() / 120000000) * 100).toFixed(2) + '%' )
+        console.log()
+        console.log('Unclaimed', new Intl.NumberFormat("en-US", { style: 'currency', currency: 'USD' }).format(120000000 - totalForgone.add(totalClaimed).toNumber()).replace('$', 'DRIFT '))
+        console.log('% Unclaimed', (((120000000 - totalClaimed.add(totalForgone).toNumber()) / 120000000) * 100).toFixed(2) + '%' )
     }
 })();
